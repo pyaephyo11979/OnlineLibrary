46ddfc4d4085d3de266e5b662b1b3c245c72950a
pyaephyo11979 <www.pyaephyoehlaing16@gmail.com>
2023-07-26 17:13:15 +0630

    Update Codes

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,18 @@
 let container=document.querySelector('body');
 let themeChanger=document.getElementById('themeBtn');
+let loginPage=document.querySelector('#page1');
+let signUpPage=document.querySelector('#page2');
+let createAccBtn=document.getElementById('createAccBtn');
+let loginAccBtn=document.getElementById('loginAccBtn');
+signUpPage.style.display="none"
+createAccBtn.addEventListener('click',()=>{
+    loginPage.style.display="none";
+    signUpPage.style.display="block";
+})
+loginAccBtn.addEventListener('click',()=>{
+    signUpPage.style.display="none";
+    loginPage.style.display="block";
+})
 function dark(){
     container.setAttribute('data-bs-theme','dark');
 }
